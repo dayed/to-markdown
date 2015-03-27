666e60a9139b05f55560f89293df906c1f4a458b
Dom Christie <christiedom@gmail.com>
2015-03-27 21:39:37 +0000

    Transfer node attributes to replacement text node

--- index.js
+++ index.js
@@ -131,6 +131,7 @@ function replacementForNode(node, doc) {
     if (canConvertNode(node, converter.filter)) {
       var replacement = converter.replacement;
       var text;
+      var textNode;
       var leadingSpace = '';
       var trailingSpace = '';
 
@@ -153,8 +154,10 @@ function replacementForNode(node, doc) {
       }
 
       text = replacement.call(toMarkdown, decodeHTMLEntities(node.innerHTML), node);
+      textNode = doc.createTextNode(leadingSpace + text + trailingSpace);
+      textNode._attributes = node.attributes;
 
-      return doc.createTextNode(leadingSpace + text + trailingSpace);
+      return textNode;
     }
   }
   return null;
